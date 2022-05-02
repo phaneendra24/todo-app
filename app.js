20879a278a8a52442834b7a101aa594156ee5289
phaneeendra_pilli_ <94274447+phaneendra24@users.noreply.github.com>
2022-05-02 10:20:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,64 +1,75 @@
-const btn = document.querySelector('.add-btn')
-const inputField = document.querySelector('input')
+const btn =document.querySelector('.add-btn')
+const inputvalue =document.querySelector('input')
 const todoList = document.querySelector('.todo-list')
 
-let todoArray = []
-btn.addEventListener('click',(e)=>
-{
-    e.preventDefault();
-    objects();
-    print()
-})
+const array = []
 
-function objects(){
-    if(inputField.value !==""){
-        
-        const arrayobject ={
-            text : inputField.value,
-            checkmark : false,
-            id : Date.now(),
+btn.addEventListener(
+    'click',(e)=>{
+        e.preventDefault()
+        if(inputvalue.value===""){
+            alert("please enter the value")
         }
-        todoArray.push(arrayobject);
-        
-        console.log(todoArray);
+        else{
+            let input = inputvalue.value
+            const arrayObj = {
+            todoValue : input,
+            id: Date.now()
+        }
+        array.push(arrayObj)
+        //iterating through the array to create dom elements
+         for (let index = array.length-1; index <= array.length-1;index++) 
+         {
+                const element = array[index].todoValue;
+                const tododiv = document.createElement('div')
+                tododiv.classList.add('div')
+                const listedItem = document.createElement('li')
+                listedItem.classList.add("liStyle")
+                listedItem.innerText = element
+                tododiv.appendChild(listedItem)
+                const delbtn = document.createElement('button')
+                delbtn.classList.add("deleteStyle")
+                delbtn.innerHTML ="delete"
+                delbtn.id = array[index].id
+                tododiv.id = delbtn.id            
+                delbtn.addEventListener('click',(e)=>{
+                const process = e.target.id
+                e.preventDefault
+                //getting the desired position in the array to delete
+                const getDelIndex = deletion(process)
+                for (let index = 0; index < array.length; index++) {
+                    if(index === getDelIndex)
+                    array.splice(index,1)
+                    
+                }
+                // deleting dom with response to array
+                if(process === tododiv.id){
+                    tododiv.remove()
+                }
+                console.log(array);                
+            }
+            )
+            //deleting the element in the array
+            function deletion(x)
+                {
+                    y=parseInt(x)
+                    for(let i=0;i<=array.length-1;i++){
+                    const flag = array[i].id
+                    const idholder = parseInt(flag)
+                    if(idholder===y)
+                    {
+                        return i
+                    }
+                    
+                }
+            }
+            tododiv.appendChild(delbtn)
+            todoList.appendChild(tododiv)
+            
+        }
+        inputvalue.value=''
+        console.log(array)
     }
-}
-function print(){
-    if(inputField.value===""){
-        alert("please enter your todo")
     }
-    else{
-        todoArray.forEach((i)=>{
-            const div =document.createElement('div')
-            div.classList.add("content-div")
-            const li =document.createElement('li')
-            li.classList.add('.style-li')
-            li.innerText=i.text
-            div.appendChild(li)
-            const completedBtn = document.createElement('button')
-            completedBtn.innerText="check"
-            completedBtn.classList.add('checkbtn')
-            completedBtn.addEventListener('click',()=>{
-                const text = li.innerText
-                const flag =text.strike();
-                li.innerHTML=flag
-            })
-            div.appendChild(completedBtn)
-            let trashbtn = document.createElement('button')
-            trashbtn.innerText="delete"
-            trashbtn.classList.add('trashButton')
-            trashbtn.addEventListener('click',(
-                )=>{
-                div.remove()
-            })
-            div.appendChild(trashbtn)
-            todoList.appendChild(div)
-        })
-        todoArray=[];
-        inputField.value=''
-    }
-}
-
-
-
-
+    )
+    
